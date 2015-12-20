534ebd60b090a587aa0e66d90ef282587fe8c3d4
MohitMalh <mohitm91@bu.edu>
2015-12-19 19:00:10 -0500

    html and query pages

---
+++ js-insta/instagram/controller3.js
@@ -0,0 +1,30 @@
+
+app.controller('InstagramCtrl', ['$scope', 'Instagram',
+	function($scope, Instagram) {
+
+		$scope.example1 = {
+			hash: 'bostonmuseumofscience'
+		};
+		
+		var instagramSuccess = function(scope, res) {
+			if (res.meta.code !== 200) {
+				scope.error = res.meta.error_type + ' | ' + res.meta.error_message;
+				return;
+			}
+			if (res.data.length > 0) {
+				scope.items = res.data;
+			} else {
+				scope.error = "This hashtag has returned no results";
+			}
+		};
+
+		Instagram.get(25,$scope.example1.hash).success(function(response) {
+			instagramSuccess($scope.example1, response);
+		});
+
+		
+
+		
+
+	}
+]);
